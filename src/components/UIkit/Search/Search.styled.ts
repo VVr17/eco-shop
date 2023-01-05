79dfdd4f54c4992d77b689b5ff52d95eaba0aba4
Anton <a.chertok@akatech.com.ua>
2023-01-05 18:18:34 +0200

    Search styles

---
+++ src/components/UIkit/Search/Search.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const SearchForm = styled("form")``;
+
+export const SearchField = styled("input")``;
+
+export const SearchButton = styled("button")``;
--- src/components/UIkit/Search/Search.tsx
+++ src/components/UIkit/Search/Search.tsx
@@ -1,5 +1,21 @@
-const Search = () => {
-  return <div>search!</div>;
+import { FC } from "react";
+import { IBaseProps, UI_BASE_PROPS } from "../base/uiBaseProps";
+
+interface ISearchProps extends IBaseProps {
+  placeholder: string;
+}
+
+const Search: FC<ISearchProps> = ({ placeholder, ...rest }) => {
+  const commonProps = { ...UI_BASE_PROPS, ...rest };
+
+  return (
+    <form>
+      <input type="text" placeholder={placeholder} {...commonProps} />
+      <button type="submit">
+        <span>icon</span>
+      </button>
+    </form>
+  );
 };
 
 export default Search;
