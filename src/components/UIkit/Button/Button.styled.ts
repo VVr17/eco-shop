9e8854d7b09d6fa4e9fbb87d08a36806e222369f
Vira Voronova <vera.voronova@hotmail.com>
2023-01-05 15:13:36 +0100

    merging

--- src/components/UIkit/Button/Button.styled.ts
+++ src/components/UIkit/Button/Button.styled.ts
@@ -1,37 +1,5 @@
 import styled from "styled-components";
-<<<<<<< HEAD
-import { IBaseProps } from "../base/uiBaseProps";
-
-export const ButtonBackdrop = styled("span")`
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-
-  background-color: rgb(0, 0, 0);
-  opacity: 0;
-  transition: opacity ${(p) => p.theme.transitionTiming};
-`;
-
-// {
-//   backgroundColor: string;
-//   color: string;
-//   borderColor: string;
-//   borderRadius: string;
-//   pl: string;
-//   pr: string;
-//   pt: string;
-//   pb: string;
-//   width: string;
-//   height: string;
-//   fontSize: string;
-// }
-=======
 import { getUIBaseStyles, IBaseProps } from "../base/uiBaseProps";
->>>>>>> main
 
 export const StyledButton = styled("button")<IBaseProps>`
   margin: 0;
