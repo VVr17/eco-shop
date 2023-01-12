301b0c05d3cfaada416203628b3b2c679369801a
Anton <a.chertok@akatech.com.ua>
2023-01-12 12:34:53 +0200

    modal cart styles

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -4,10 +4,30 @@ import Button from "components/UIkit/Button";
 const Cart = () => {
   return (
     <>
-      <Box as="h2">My shopping cart:</Box>
+      <Box as="h2" fontSize="s" lineHeight="1.375">
+        My shopping cart:
+      </Box>
       <Box as="ul"></Box>
-      <Box as="p"></Box>
-      <Button text="To checkout" backgroundColor="accent" width="100%" />
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        mt="24px"
+      >
+        <Box as="p" fontSize="18px" lineHeight="1.22" fontWeight="bold">
+          Subtotal:
+        </Box>
+        <Box as="p" fontSize="18px" lineHeight="1.22" fontWeight="bold">
+          $72.00
+        </Box>
+      </Box>
+      <Button
+        text="To checkout"
+        backgroundColor="accent"
+        width="100%"
+        color="lightText"
+        mt="24px"
+      />
     </>
   );
 };
