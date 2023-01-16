c66a5e4159ff0b0c2da9e527833158084e752bb5
Anton <a.chertok@akatech.com.ua>
2023-01-16 21:06:48 +0200

    Update checkout.tsx

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,3 +1,5 @@
+import Box from "components/Box";
+import Container from "components/Container";
 import Heading from "components/Heading";
 import Section from "components/Section";
 import Head from "next/head";
@@ -9,6 +11,11 @@ const Checkout = () => (
       <meta name="description" content="eco shop" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
+    <Section>
+      <Container>
+        <Box>Homepage/Checkout</Box>
+      </Container>
+    </Section>
     <Section>
       <Heading tag="h2" text="Checkout" />
     </Section>
