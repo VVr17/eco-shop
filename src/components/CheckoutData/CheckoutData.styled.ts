0b53d2a14e8a4a952fe5dbc9d94dc21e3a7d8c88
Anton <a.chertok@akatech.com.ua>
2023-01-17 18:25:57 +0200

    remake checkout page

--- src/components/CheckoutData/CheckoutData.styled.ts
+++ src/components/CheckoutData/CheckoutData.styled.ts
@@ -1,14 +1,27 @@
 import styled from "styled-components";
 
 export const H3 = styled("h3")`
+  margin-bottom: 24px;
+
   font-size: ${(p) => p.theme.fontSizes.m};
   line-height: 1.1;
   font-weight: ${(p) => p.theme.fontWeight.medium};
   color: ${(p) => p.theme.colors.mainText};
 `;
 
-export const FieldSet = styled("div")``;
+export const FieldSet = styled("div")`
+  padding-bottom: 56px;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: ${(p) => p.theme.space[4]};
+  row-gap: ${(p) => p.theme.space[3]};
+`;
 
-export const FieldWrapper = styled("div")``;
+export const FieldWrapper = styled("div")`
+  flex-basis: calc((100% - ${(p) => p.theme.space[4]}) / 2);
+`;
 
-export const Label = styled("label")``;
+export const Label = styled("label")`
+  display: block;
+  margin-bottom: ${(p) => p.theme.space[2]};
+`;
--- src/components/CheckoutData/CheckoutData.tsx
+++ src/components/CheckoutData/CheckoutData.tsx
@@ -37,7 +37,118 @@ const CheckoutData = () => {
     <>
       <Box display="flex" justifyContent="space-between">
         <Box width="712px" border="1px solid grey">
-          {checkoutSections.map(({ heading, fieldSet }) => (
+          <Box>
+            <H3>Personal information:</H3>
+            <FieldSet>
+              <FieldWrapper>
+                <Label htmlFor="checkout_first_name">First name</Label>
+                <Field
+                  id="checkout_first_name"
+                  name="first_name"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_last_name">Last name</Label>
+                <Field
+                  id="checkout_last_name"
+                  name="last_name"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_phone">Phone</Label>
+                <Field
+                  id="checkout_phone"
+                  name="phone"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_email">Email</Label>
+                <Field
+                  id="checkout_email"
+                  name="email"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+            </FieldSet>
+          </Box>
+          <Box>
+            <H3>Delivery details:</H3>
+            <FieldSet>
+              <FieldWrapper>
+                <Label htmlFor="checkout_country">Country / Region</Label>
+                <Select
+                  id="checkout_country"
+                  list={countries}
+                  fontSize="15px"
+                  width="100%"
+                  name="country"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_city">Town / City</Label>
+                <Select
+                  id="checkout_city"
+                  list={selectLists.city}
+                  fontSize="15px"
+                  width="100%"
+                  name="city"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_street">Street</Label>
+                <Field
+                  id="checkout_street"
+                  name="street"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_postcode">Postcode</Label>
+                <Field
+                  id="checkout_postcode"
+                  name="postcode"
+                  type="text"
+                  fontSize="15px"
+                  width="100%"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_packaging">Packaging type</Label>
+                <Select
+                  id="checkout_packaging"
+                  list={selectLists.packaging}
+                  fontSize="15px"
+                  width="100%"
+                  name="packaging"
+                />
+              </FieldWrapper>
+              <FieldWrapper>
+                <Label htmlFor="checkout_shipping">Shipping option</Label>
+                <Select
+                  id="checkout_shipping"
+                  list={selectLists.shipping}
+                  fontSize="15px"
+                  width="100%"
+                  name="shipping"
+                />
+              </FieldWrapper>
+            </FieldSet>
+          </Box>
+
+          {/* {checkoutSections.map(({ heading, fieldSet }) => (
             <Box>
               <H3>{heading}</H3>
               <FieldSet>
@@ -45,19 +156,35 @@ const CheckoutData = () => {
                   <FieldWrapper>
                     <Label htmlFor={id}>{label}</Label>
                     {type === "text" && (
-                      <Field id={id} name={name} type="text" />
+                      <Field
+                        id={id}
+                        name={name}
+                        type="text"
+                        fontSize="15px"
+                        width="100%"
+                      />
                     )}
                     {type === "select" && (
-                      <Select list={selectLists[name as keyof any]} />
+                      <Select
+                        list={selectLists[name as keyof any]}
+                        fontSize="15px"
+                        width="100%"
+                      />
                     )}
                     {type === "password" && (
-                      <Field id={id} name={name} type="password" />
+                      <Field
+                        id={id}
+                        name={name}
+                        type="password"
+                        fontSize="15px"
+                        width="100%"
+                      />
                     )}
                   </FieldWrapper>
                 ))}
               </FieldSet>
             </Box>
-          ))}
+          ))} */}
         </Box>
         <Box width="370px" border="1px solid grey" backgroundColor="grey">
           Your order:
