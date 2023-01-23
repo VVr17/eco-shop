610ee74975c464593f2adca0f7e7bf68fb1e109d
Vira Voronova <vera.voronova@hotmail.com>
2023-01-23 12:10:28 +0100

    filter state done

--- src/components/Filter/FilterType/FilterList/FilterList.tsx
+++ src/components/Filter/FilterType/FilterList/FilterList.tsx
@@ -1,13 +1,36 @@
 import Checkbox from "components/UIkit/Checkbox";
+
+import { FiSearch } from "react-icons/fi";
 import { List } from "./FilterList.styled";
 
 interface IProps {
   listItems?: string[];
+  withSearch?: boolean;
+  onCheckboxChange: (value: string, checked: boolean) => void;
 }
 
-const FilterList: React.FC<IProps> = ({ listItems }) => {
+const FilterList: React.FC<IProps> = ({
+  listItems,
+  withSearch = false,
+  onCheckboxChange,
+}) => {
   return (
     <>
+      {withSearch && (
+        <>
+          <input
+            type="text"
+            name="search"
+            // value={searchValue}
+            // onChange={onSearchHandler}
+            // placeholder={placeholder}
+            // {...commonProps}
+          />
+          <button type="submit">
+            <FiSearch size={"18px"} />
+          </button>
+        </>
+      )}
       {listItems && (
         <List>
           {listItems.map((item) => (
@@ -16,7 +39,7 @@ const FilterList: React.FC<IProps> = ({ listItems }) => {
                 label={item}
                 initialChecked={false}
                 onChange={(checked) => {
-                  console.log("checked", item, checked);
+                  onCheckboxChange(item, checked);
                 }}
               />
             </li>
