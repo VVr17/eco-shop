41aa58ce24679d4681d1f7a82552131238b6e016
Anton <a.chertok@akatech.com.ua>
2023-02-21 13:47:46 +0200

    cards

--- src/components/WhatToCook/WhatToCook.styled.ts
+++ src/components/WhatToCook/WhatToCook.styled.ts
@@ -1,5 +1,7 @@
-import { theme } from "constants/theme";
 import styled from "styled-components";
+import otherReciepsBgd from "assets/images/whatToCook/otherReciepsBgd.png";
+import otherReciepsBgdSmall from "assets/images/whatToCook/otherReciepsBgdSmall.png";
+import { theme } from "constants/theme";
 
 export const MainWrapper = styled("div")`
   width: 100%;
@@ -36,9 +38,30 @@ const CardItem = styled("div")`
   }
 `;
 
-export const Recipe = styled(CardItem)`
+export const Recipe = styled(CardItem)<{
+  backgroundUrl: string;
+  backgroundColor: string;
+}>`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 56px;
+
   border-radius: 24px;
-  background-color: coral;
+  background: url(${(p) => p.backgroundUrl});
+  background-repeat: no-repeat;
+  background-size: 60%;
+  background-position: center;
+  background-color: ${(p) => p.backgroundColor};
+
+  ${theme.mq.mobileOnly} {
+    padding: 24px 184px 24px 32px;
+    background-size: 50%;
+    background-position: top 50% right 20px;
+    align-items: flex-start;
+  }
 `;
 
 export const RestCardsContainer = styled(CardItem)`
@@ -78,9 +101,64 @@ const SmallCard = styled("div")`
 `;
 
 export const CardOtherRecipes = styled(SmallCard)`
-  background-color: aqua;
+  padding: 32px 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+
+  background: url(${otherReciepsBgd.src});
+  background-color: #4c7c7d;
+  background-repeat: no-repeat;
+  background-position: right;
+
+  ${theme.mq.mobileOnly} {
+    padding: 18px 32px;
+    background: url(${otherReciepsBgdSmall.src});
+    background-color: #4c7c7d;
+    background-repeat: no-repeat;
+    background-position: right;
+  }
 `;
 
 export const CardSubscribeForRecipes = styled(SmallCard)`
-  background-color: bisque;
+  background-color: #edeae7;
+`;
+
+// export const ReceipeTitleWrapper = styled("div")`
+
+// `;
+
+export const RecipeTitle = styled("h3")<{ color: string }>`
+  margin-bottom: 16px;
+
+  font-size: 32px;
+  line-height: 1;
+  letter-spacing: 0.02em;
+
+  text-align: center;
+
+  color: ${(p) => p.color};
+
+  ${theme.mq.mobileOnly} {
+    margin-bottom: 6px;
+    text-align: left;
+    font-size: 16px;
+    line-height: 1.5;
+  }
+`;
+
+export const ReceipeAuthor = styled("p")`
+  text-align: center;
+  font-size: 13px;
+  line-height: 1.692;
+  text-transform: uppercase;
+  letter-spacing: 0.12em;
+  color: rgba(56, 54, 52, 0.9);
+
+  ${theme.mq.mobileOnly} {
+    text-align: left;
+    font-size: 9px;
+    line-height: 1.777;
+  }
 `;
--- src/components/WhatToCook/WhatToCook.tsx
+++ src/components/WhatToCook/WhatToCook.tsx
@@ -1,6 +1,9 @@
 import Container from "components/Container";
 import HomePageContainer from "components/HomePageContainer";
 import HomePageTitle from "components/HomePageTitle";
+import watermalonPic from "assets/images/whatToCook/watermelon.png";
+import asparagusPic from "assets/images/whatToCook/asparagus.png";
+
 import { WhatToCookIcon } from "assets/icons/homePageIcons";
 
 import {
@@ -9,20 +12,89 @@ import {
   RestCardsContainer,
   CardOtherRecipes,
   CardSubscribeForRecipes,
+  RecipeTitle,
+  ReceipeAuthor,
 } from "./WhatToCook.styled";
+import Image from "next/image";
+import Box from "components/Box";
+import { Button } from "components/UIkit";
+import { useWindowSize } from "hooks/useWindowSize";
 
 const WhatToCook = () => {
+  const { isDesktop, isMobile, isTablet } = useWindowSize();
+
   return (
     <HomePageContainer>
       <Container>
         <HomePageTitle title="What to cook?" icon={WhatToCookIcon} />
 
         <MainWrapper>
-          <Recipe>recipe1</Recipe>
-          <Recipe>recipe2</Recipe>
+          <Recipe
+            backgroundUrl={watermalonPic.src}
+            backgroundColor="rgba(228, 89, 89, 0.1)"
+          >
+            <Box>
+              <RecipeTitle color="#C64444">Watermelon</RecipeTitle>
+              <ReceipeAuthor>Recipes with Anna Grace</ReceipeAuthor>
+            </Box>
+            <Button
+              text="Read more"
+              backgroundColor="transparent"
+              color="#E45959"
+              borderColor="#E45959"
+              fontSize={isMobile ? "14px" : "15px"}
+              pt={isMobile ? "10px" : "12px"}
+              pb={isMobile ? "10px" : "12px"}
+              pl={isMobile ? "10px" : "20px"}
+              pr={isMobile ? "10px" : "20px"}
+            />
+          </Recipe>
+
+          <Recipe
+            backgroundUrl={asparagusPic.src}
+            backgroundColor="rgba(76, 124, 125, 0.1)"
+          >
+            <Box>
+              <RecipeTitle color="#355F60">Asparagus</RecipeTitle>
+              <ReceipeAuthor>Recipes with Anna Grace</ReceipeAuthor>
+            </Box>
+            <Button
+              text="Read more"
+              backgroundColor="transparent"
+              color="#4C7C7D"
+              borderColor="#4C7C7D"
+              fontSize={isMobile ? "14px" : "15px"}
+              pt={isMobile ? "10px" : "12px"}
+              pb={isMobile ? "10px" : "12px"}
+              pl={isMobile ? "10px" : "20px"}
+              pr={isMobile ? "10px" : "20px"}
+            />
+          </Recipe>
 
           <RestCardsContainer>
-            <CardOtherRecipes>other recipes</CardOtherRecipes>
+            <CardOtherRecipes>
+              <Box>
+                <Box
+                  as="h3"
+                  color="input"
+                  mb={10}
+                  fontSize={isMobile ? "26px" : "36px"}
+                >
+                  200+
+                </Box>
+                <Box as="p" color="input" fontSize={isMobile ? "17px" : "20px"}>
+                  Healthy recipes
+                </Box>
+              </Box>
+              <Button
+                text="View all"
+                backgroundColor="transparent"
+                color="mainBackground"
+                borderColor="mainBackground"
+                fontSize="15px"
+                width="160px"
+              />
+            </CardOtherRecipes>
             <CardSubscribeForRecipes>
               subscribeToRecipes
             </CardSubscribeForRecipes>
